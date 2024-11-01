4dea985b0c15c3abd310baa2099430bce337625c
Alexander Mendivil <alexanderima@outlook.es>
2024-10-31 18:22:55 -0700

    Added idle state to enemy

--- scripts/monster/skeleton_warrior_monster/skeleton_warrior_monster.gd
+++ scripts/monster/skeleton_warrior_monster/skeleton_warrior_monster.gd
@@ -1,13 +1,29 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@onready var state_controller = $StateMachine
 
+@export var player: CharacterBody3D
+
+const speed: float = 1.0
+var direction: Vector3
+var is_awake: bool = false
+var is_attacking: bool = false
+var health: int = 4
+var damage: int = 2
+var is_dying: bool = false
+var just_hit: bool = false
+
+func _ready() -> void:
+	state_controller.change_state("idle")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if player:
+		direction = (player.global_transform.origin - global_transform.origin).normalized()
+
+	
 	move_and_slide()
--- scripts/monster/states_machine/idle_state.gd
+++ scripts/monster/states_machine/idle_state.gd
@@ -3,5 +3,12 @@ var StateController
 
 func _ready() -> void:
 	StateController = get_parent().get_parent()
+	if StateController.is_awake:
+		await StateController.get_node("AnimationTree").animation_finished
 
+	StateController.get_node("AnimationTree").get("parameters/playback").travel("Sit_Floor_Idle")
 	
+func _physics_process(delta: float) -> void:
+	if StateController:
+		StateController.velocity.x = 0
+		StateController.velocity.z = 0
